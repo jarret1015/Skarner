9544ed2948aa22e88129be3eb24867f8160fc0e1
jarret1015 <1043846252@qq.com>
2019-09-11 11:50:57 +0800

    init

---
+++ src/main/java/site/jarret/skarner/SkarnerApplication.java
@@ -0,0 +1,13 @@
+package site.jarret.skarner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkarnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkarnerApplication.class, args);
+    }
+
+}
